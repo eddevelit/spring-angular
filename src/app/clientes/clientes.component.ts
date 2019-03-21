53acbc19174ae195b128bb5a1969e30c1abee018
Eddevelit <edd.develit@gmail.com>
2019-03-21 16:30:41 -0600

    Creando archivo clientes.json.ts con la lista de objetos Se crea el archivo clientes.json.ts para obtener de ahi los datos en vez de obtenerlos de el arreglo en lientes.component..ts

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente} from './cliente';
+import { CLIENTES} from './clientes.json';
 
 @Component({
   selector: 'app-clientes',
@@ -8,15 +9,10 @@ import { Cliente} from './cliente';
 export class ClientesComponent implements OnInit {
 
   constructor() { }
-  clientes: Cliente[] = [
-    {id: 1, nombre: 'Edu', apellido: 'Awesome', email: 'edd.develit@gmail.com', createAt: '2019-03-21'},
-    {id: 2, nombre: 'Eddo', apellido: 'Lagunas', email: 'edd.deve@gmail.com', createAt: '2019-03-10'},
-    {id: 3, nombre: 'Jorge', apellido: 'Ocampo', email: 'jorge.develit@gmail.com', createAt: '2019-08-11'},
-    {id: 4, nombre: 'George', apellido: 'Waters', email: 'george.develit@gmail.com', createAt: '2019-06-08'},
-    {id: 5, nombre: 'Lalo', apellido: 'Lennon', email: 'lalo.landa@gmail.com', createAt: '2019-04-15'}
-  ];
+  clientes: Cliente[] = [];
 
   ngOnInit() {
+    this.clientes = CLIENTES;
   }
 
 }
