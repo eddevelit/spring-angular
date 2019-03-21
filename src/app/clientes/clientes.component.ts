dbe2f2e3b16aaaec1d77852af6176a880e4f8950
Eddevelit <edd.develit@gmail.com>
2019-03-21 16:17:00 -0600

    Listando los objetos del tipo Cliente Se modificaron los archivos del componente cliente para mostrar en front un listado de clientes almacenados en un arreglo ubicado en clientes.component.ts

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente} from './cliente';
 
 @Component({
   selector: 'app-clientes',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ClientesComponent implements OnInit {
 
   constructor() { }
+  clientes: Cliente[] = [
+    {id: 1, nombre: 'Edu', apellido: 'Awesome', email: 'edd.develit@gmail.com', createAt: '2019-03-21'},
+    {id: 2, nombre: 'Eddo', apellido: 'Lagunas', email: 'edd.deve@gmail.com', createAt: '2019-03-10'},
+    {id: 3, nombre: 'Jorge', apellido: 'Ocampo', email: 'jorge.develit@gmail.com', createAt: '2019-08-11'},
+    {id: 4, nombre: 'George', apellido: 'Waters', email: 'george.develit@gmail.com', createAt: '2019-06-08'},
+    {id: 5, nombre: 'Lalo', apellido: 'Lennon', email: 'lalo.landa@gmail.com', createAt: '2019-04-15'}
+  ];
 
   ngOnInit() {
   }
