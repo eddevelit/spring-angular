66bb357b99470898c66909f3b065a1ba1a057d01
Eddevelit <edd.develit@gmail.com>
2019-03-25 16:04:08 -0600

    Creando el componente form.component y la vista del formulario

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from './cliente';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: []
+})
+export class FormComponent implements OnInit {
+
+  private cliente: Cliente = new Cliente();
+  private titulo = 'Crear Cliente';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public create(): void{
+    console.log('Clicked!');
+    console.log(this.cliente);
+  }
+}
