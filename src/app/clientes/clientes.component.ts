f0e45c30e726002b61391d9b17b2fd9f0c10000d
Eddevelit <edd.develit@gmail.com>
2019-03-26 13:52:06 -0600

    Escrbiendo el delete en la clase service y en el componenente clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente} from './cliente';
 import {ClienteService} from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -14,6 +15,40 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getclientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+      },
+      buttonsStyling: false,
+    })
+
+    swalWithBootstrapButtons.fire({
+      title: 'Está seguro?',
+      text: `Se eliminará el cliente ${cliente.nombre} ${cliente.apellido}`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, Borrar!',
+      cancelButtonText: 'No, Cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+
+        this.clienteService.delete(cliente.id).subscribe(
+          response => {
+            this.clientes = this.clientes.filter(cli => cli !== cliente);
+            swalWithBootstrapButtons.fire(
+              'Eliminado!',
+              `Cliente ${cliente.nombre} ${cliente.apellido} eliminado con éxito`,
+              'success'
+            );
+          }
+        );
+      }
+    });
   }
 
 }
