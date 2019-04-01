30229b5bea6a5291305d4474ba57ace0ae27cf55
Eddevelit <edd.develit@gmail.com>
2019-04-01 11:53:41 -0600

    Creando el componente paginador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FormComponent } from './clientes/form.component';
+import { PaginatorComponent } from './paginator/paginator.component';
 import {ClienteService} from './clientes/cliente.service';
 import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
-import { FormComponent } from './clientes/form.component';
 import { FormsModule } from '@angular/forms';
 import {registerLocaleData} from '@angular/common';
 import localEs from '@angular/common/locales/es-MX';
+
 
 registerLocaleData(localEs, 'es');
 
@@ -32,7 +34,8 @@ const routes: Routes = [
     FooterComponent,
     DirectivaComponent,
     ClientesComponent,
-    FormComponent
+    FormComponent,
+    PaginatorComponent
   ],
   imports: [
     BrowserModule,
