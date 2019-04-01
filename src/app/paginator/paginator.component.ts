42832e98dad157929445e03309427987f7edf583
Eddevelit <edd.develit@gmail.com>
2019-04-01 16:10:49 -0600

    Corrigiendo if del paginador

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
@@ -15,18 +15,25 @@ export class PaginatorComponent implements OnInit, OnChanges {
   constructor() { }
 
   ngOnInit() {
+    this.initPaginator();
   }
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges): void {
+    const paginadorActualizado = changes['paginador'];
+    if (paginadorActualizado.previousValue) {
+    this.initPaginator();
+    }
+  }
+  private initPaginator(): void {
     this.desde = Math.min( Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5) ;
     this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
     if (this.paginador.totalPages > 5) {
-      this.paginas = new Array(this.paginador.totalPages)
+      this.paginas = new Array(this.hasta - this.desde +1 )
         .fill(0)
-        .map((valor, indice) => indice + this.desde);
+        .map((_valor, indice) => indice + this.desde);
     } else {
       this.paginas = new Array(this.paginador.totalPages)
         .fill(0)
-        .map((valor, indice) => indice + 1);
+        .map((_valor, indice) => indice + 1);
     }
   }
 }
