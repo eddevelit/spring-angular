2281d7196a4be14c9a632af7f6b722e322d4982a
Eddevelit <edd.develit@gmail.com>
2019-04-01 11:54:48 -0600

    Agregando carpeta de componente paginator olvidada

---
+++ src/app/paginator/paginator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'paginator-nav',
+  templateUrl: './paginator.component.html',
+  styleUrls: []
+})
+export class PaginatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
