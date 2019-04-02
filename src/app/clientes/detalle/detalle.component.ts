3e73ee7b02a38a25a932333a4aa0db0f7eed5aa6
Eddevelit <edd.develit@gmail.com>
2019-04-02 17:40:00 -0600

    Validando imagen antes de subir

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -28,15 +28,24 @@ private fotoSeleccionada: File;
   seleccionarFoto(event){
     this.fotoSeleccionada = event.target.files[0];
     console.log(this.fotoSeleccionada);
+
+    if (this.fotoSeleccionada.type.indexOf('image') < 0) {
+      swal.fire('Error seleccionar imagen: ', 'El archivo debe ser de tipo imagen', 'error');
+      this.fotoSeleccionada = null;
+    }
   }
 
-  subirFoto(){
-    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(
-      cliente => {
-         this.cliente = cliente;
-         swal.fire('La foto se ha subido completamente!', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
-      });
+  subirFoto() {
 
+    if (!this.fotoSeleccionada){
+      swal.fire('Error:', 'Debe subir una foto', 'error' );
+    } else {
+      this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(
+        cliente => {
+          this.cliente = cliente;
+          swal.fire('La foto se ha subido completamente!', `La foto se ha subido con éxito: ${this.cliente.foto}`, 'success');
+        });
+    }
   }
 
 }
