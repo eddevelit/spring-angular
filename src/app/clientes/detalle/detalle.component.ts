2a95320b089e61229986a6690bf8983241454c8f
Eddevelit <edd.develit@gmail.com>
2019-04-02 16:04:13 -0600

    Anadiendo componente detalle para el upload en el front-end

---
+++ src/app/clientes/detalle/detalle.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Cliente} from '../cliente';
+import {ClienteService} from '../cliente.service';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'detalle-cliente',
+  templateUrl: './detalle.component.html',
+  styleUrls: ['./detalle.component.css']
+})
+export class DetalleComponent implements OnInit {
+cliente: Cliente;
+titulo = 'Detalle del cliente';
+  constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => { const id: number = + params.get('id');
+                                                       if (id) {
+      this.clienteService.getCliente(id).subscribe(cliente => {
+        this.cliente = cliente;
+      });
+    }
+    });
+  }
+
+}
