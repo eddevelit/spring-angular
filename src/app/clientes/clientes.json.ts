e2a57c68c51ad2ffab60fdb268fbf75fde1a63bc
Eddevelit <edd.develit@gmail.com>
2019-04-03 11:02:36 -0600

    Añadiedo barra de progreso

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -1,9 +1,4 @@
 import {Cliente} from './cliente';
 
 export const CLIENTES: Cliente[] = [
-  {id: 1, nombre: 'Edu', apellido: 'Awesome', email: 'edd.develit@gmail.com', createAt: '2019-03-21'},
-  {id: 2, nombre: 'Eddo', apellido: 'Lagunas', email: 'edd.deve@gmail.com', createAt: '2019-03-10'},
-  {id: 3, nombre: 'Jorge', apellido: 'Ocampo', email: 'jorge.develit@gmail.com', createAt: '2019-08-11'},
-  {id: 4, nombre: 'George', apellido: 'Waters', email: 'george.develit@gmail.com', createAt: '2019-06-08'},
-  {id: 5, nombre: 'Lalo', apellido: 'Lennon', email: 'lalo.landa@gmail.com', createAt: '2019-04-15'}
 ];
