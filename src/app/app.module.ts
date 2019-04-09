806d95a4317750602f1b5ddc687bbefe6cee09da
Eddevelit <edd.develit@gmail.com>
2019-04-09 16:19:20 -0500

    Anadiendo seguridad en las rutas con Guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import {MatDatepicker, MatDatepickerModule, MatNativeDateModule} from '@angular/
 import {MatMomentDateModule} from '@angular/material-moment-adapter';
 import { DetalleComponent } from './clientes/detalle/detalle.component';
 import { LoginComponent } from './usuarios/login.component';
+import {AuthGuard} from './usuarios/guards/auth.guard';
 
 
 registerLocaleData(localEs, 'es');
@@ -28,8 +29,8 @@ const routes: Routes = [
   {path: 'directivas', component: DirectivaComponent},
   {path: 'clientes', component: ClientesComponent},
   {path: 'clientes/page/:page', component: ClientesComponent},
-  {path: 'clientes/form', component: FormComponent},
-  {path: 'clientes/form/:id', component: FormComponent},
+  {path: 'clientes/form', component: FormComponent, canActivate:[AuthGuard]},
+  {path: 'clientes/form/:id', component: FormComponent, canActivate:[AuthGuard]},
   {path: 'login', component: LoginComponent}
 ];
 
